efcab57a6cf3ee8faa180fd49a7e262f7c746261
ussenuk <ukimanuka@gmail.com>
2024-03-09 17:23:57 +0300

    worked on getting remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,21 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        json_list = []
+        items = json.loads(self.get_response_body())
+        # for item in items:
+        #     json_list.append(item["name"])
+            
+        return items
+    
+# items = GetRequester(url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json").get_response_body()
+# print(items)
+  
+get_requester = GetRequester(url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+people = get_requester.load_json()
+print(people)
